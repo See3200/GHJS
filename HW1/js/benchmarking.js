c40a65d69add05efc70b12d52239cae67a70ce4a
See3200 <meer3200@gmail.com>
2017-11-07 08:59:00 +0200

    Added data for writing in benchmarking.js

--- HW1/js/benchmarking.js
+++ HW1/js/benchmarking.js
@@ -21,17 +21,22 @@ for (var i = 0; i < 10000; i++) {
 
 function walkIn(obj) {
     for (var key in obj) {
+        obj[key] = 'key' + i + i;
     }
 }
 
 function walkForArr(arr) {
     for (var i = 0; i < arr.length; i++) {
+        arr[i] = 'key' + i + i;
     }
 }
 
 function walkForObj(obj) {
+    var objKeys = Object.keys(obj);
     var length = Object.keys(obj).length;
     for (var i = 0; i < length; i++) {
+        var key = objKeys[i];
+        obj[key] = 'key' + i + i;
     }
 }
 
@@ -50,9 +55,9 @@ var timeInObj1 = bench(walkIn, obj1);
     timeForObj2 = bench(walkForObj, obj2);
     timeForArr = bench(walkForArr, arr1);
 
-alert('Время walkIn для obj1: ' + timeInObj1 + 'мс');
-alert('Время walkIn для obj2: ' + timeInObj2 + 'мс');
-alert('Время walkIn для arr1: ' + timeInArr + 'мс');
-alert('Время walkForObj для obj1: ' + timeForObj1 + 'мс');
-alert('Время walkForObj для obj2: ' + timeForObj2 + 'мс');
-alert('Время walkForArr для arr1: ' + timeForArr + 'мс');
+console.log('Время walkIn для obj1: ' + timeInObj1 + 'мс');
+console.log('Время walkIn для obj2: ' + timeInObj2 + 'мс');
+console.log('Время walkIn для arr1: ' + timeInArr + 'мс');
+console.log('Время walkForObj для obj1: ' + timeForObj1 + 'мс');
+console.log('Время walkForObj для obj2: ' + timeForObj2 + 'мс');
+console.log('Время walkForArr для arr1: ' + timeForArr + 'мс');
