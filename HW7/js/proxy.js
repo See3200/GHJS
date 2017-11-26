061b19a764cba1e9c14bcede50e2769aee4ae1b0
See3200 <meer3200@gmail.com>
2017-11-26 15:39:41 +0200

    Added proxy part

---
+++ HW7/js/proxy.js
@@ -0,0 +1,40 @@
+let weakmap = new WeakMap();
+
+function proxyClass(obj) {
+
+    return new Proxy(obj, {
+        construct: function(target, argumentsList) {
+            weakmap.set(target, argumentsList);
+            return new target(...argumentsList);
+        }
+    });
+}
+
+class CreateDiv {
+    constructor(width, height, name) {
+        this.width = width;
+        this.height = height;
+        this.name = name;
+    }
+    createElem() {
+        let div = document.createElement("div");
+        div.style.width= this.width + "px";
+        div.style.height= this.height + "px";
+        div.innerHTML = `${this.name}`;
+        document.body.appendChild(div);
+    }
+}
+
+class CreateDiv2 {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+}
+CreateDiv = proxyClass(CreateDiv);
+CreateDiv2 = proxyClass(CreateDiv2);
+
+let div1 = new CreateDiv(100, 50, "DIV");
+let div2 = new CreateDiv2(90, 45);
+
+console.log(weakmap);
